ccbad980e2a398cda0b75ee9f8e01bc9e2b3ec14
dylanR96 <dylan.ravenson@gmail.com>
2024-01-28 16:15:27 +0100

    Added most of layout and styling for mobile view, function is complete, some error handling remaining

--- script.js
+++ script.js
@@ -1,56 +1,135 @@
 let cardNum = document.getElementById('card-numbers');
 let inputNum = document.getElementById('card-num');
-let enteredNumber = document.getElementById('entered-number');
-let cardFront = document.getElementById('card-front');
+let container = document.getElementById('container');
+let errorMessageNum = document.getElementById('error-message-card-num');
 
 let cardName = document.getElementById('card-name');
 let inputName = document.getElementById('card-holder');
+let cardFrontBottom = document.getElementById('card-front-bottom');
+let errorMessageName = document.getElementById('error-message-card-name');
 
-let cardDates = document.getElementById('card-dates');
+let cardMonth = document.getElementById('card-date-month');
 let inputMonth = document.getElementById('date-month');
+let cardYear = document.getElementById('card-date-year');
 let inputYear = document.getElementById('date-year');
+let errorMessageDates = document.getElementById('error-message-dates');
 
 let cvc = document.getElementById('cvc');
 let inputCvc = document.getElementById('input-cvc');
+let errorMessageCvc = document.getElementById('error-message-cvc');
 
-inputNum.addEventListener('input', updateValue);
+inputNum.addEventListener('input', updateNum);
 inputName.addEventListener('input', updateName);
-inputMonth.addEventListener('input', updateDates);
+inputMonth.addEventListener('input', updateMonth);
+inputYear.addEventListener('input', updateYear);
 inputCvc.addEventListener('input', updateCvc);
 
-let isReplaced = false;
-addEventListener("keydown", () => {
-  if(!isReplaced) {
-  cardFront.replaceChild(enteredNumber, cardNum);
-  cardFront.classList.add('card-front__card-numbers');
-  isReplaced = true;
-  }
-});
+let original = cardNum.textContent;
+let original2 = cardName.textContent;
+let original3 = cardMonth.textContent;
+let original4 = cardYear.textContent;
+let original5 = cvc.textContent;
 
-function updateValue(e) {
+function updateNum(e) {
   const regex = /.{4}/g;
-  enteredNumber.textContent = e.target.value.replace(regex, function (x) {
-    return x + " ";
-  });
-  let maxLength = 15;
-  if(e.target.value.length > maxLength) {
-    e.target.value = e.target.value.substring(0, maxLength);
-  }
+  if(isNaN(e.target.value)) {
+    checkLength(e, 15);
+    cardNum.textContent = e.target.value.replace(regex, function (x) {
+      return x + " ";
+    })
+    errorMessages(errorMessageNum, inputNum, 'Wrong format, numbers only', 'red', '1px solid red');
+    }
+    else {
+      checkLength(e, 16);
+      cardNum.textContent = e.target.value.replace(regex, function (x) {
+        return x + " ";
+      })
+      errorMessages(errorMessageNum, inputNum, '', '', '');
+      
+    }
+    if(e.target.value == "") {
+      cardNum.textContent = original;
+    }
+  
 }
-
 
 function updateName(e) {
-  cardName.textContent = e.target.value
+  const regex = /[0-9]/g;
+  if(regex.test(e.target.value)) {
+    checkLength(e, 20);
+    cardName.textContent = e.target.value;
+    errorMessages(errorMessageName, inputName, 'Wrong format, letters only', 'red', '1px solid red');
+  }
+  else {
+    checkLength(e, 20);
+    cardName.textContent = e.target.value;
+    errorMessages(errorMessageName, inputName, '', '', '');
+  }
+  if(e.target.value == "") {
+    cardName.textContent = original2;
+  }
+  
 }
 
-function updateDates(e) {
-  cardDates.textContent = e.target.value
+function updateMonth(e) {
+  if(isNaN(e.target.value)) {
+    checkLength(e, 2);
+    cardMonth.textContent = e.target.value;
+    errorMessages(errorMessageDates, inputMonth, 'Wrong format, numbers only', 'red', '1px solid red');
+    }
+    else {
+      checkLength(e, 2);
+      cardMonth.textContent = e.target.value;
+      errorMessages(errorMessageDates, inputMonth, '', '', '');
+    }
+    if(e.target.value == "") {
+      cardMonth.textContent = original3;
+    }
+  
+}
+
+function updateYear(e) {
+  if(isNaN(e.target.value)) {
+    checkLength(e, 2);
+    cardYear.textContent = e.target.value;
+    errorMessages(errorMessageDates, inputYear, 'Wrong format, numbers only', 'red', '1px solid red');
+    }
+    else {
+      checkLength(e, 2);
+      cardYear.textContent = e.target.value;
+      errorMessages(errorMessageDates, inputYear, '', '', '');
+    }
+    if(e.target.value == "") {
+      cardYear.textContent = original4;
+    } 
 }
 
 function updateCvc(e) {
-  cvc.textContent = e.target.value
-  let maxLength = 3;
+  if(isNaN(e.target.value)) {
+    checkLength(e, 3);
+    cvc.textContent = e.target.value;
+    errorMessages(errorMessageCvc, inputCvc, 'Wrong format, numbers only', 'red', '1px solid red');
+    }
+    else {
+      checkLength(e, 3);
+      cvc.textContent = e.target.value;
+      errorMessages(errorMessageCvc, inputCvc, '', '', '');
+      
+    }
+    if(e.target.value == "") {
+      cvc.textContent = original5;
+    }
+  
+}
+
+function checkLength(e, maxLength) {
   if(e.target.value.length > maxLength) {
     e.target.value = e.target.value.substring(0, maxLength);
   }
+}
+
+function errorMessages(errorContainer, error, errorMessage, color, border) {
+  errorContainer.textContent = errorMessage;
+  errorContainer.style.color = color;
+  error.style.border = border;
 }
