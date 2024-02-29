533459f701350ed150fc772dad81d5c531864dce
dylanR96 <dylan.ravenson@gmail.com>
2024-02-29 20:08:11 +0100

    Overhaul of classes and css, minor changes in javascript

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-let cardNum = document.getElementById('card-numbers');
-let inputNum = document.getElementById('card-num');
-let container = document.getElementById('container');
-let errorMessageNum = document.getElementById('error-message-card-num');
+const cardNum = document.getElementById('card-numbers');
+const inputNum = document.getElementById('card-num');
+const errorMessageNum = document.getElementById('error-message-card-num');
 
-let cardName = document.getElementById('card-name');
-let inputName = document.getElementById('card-holder');
-let cardFrontBottom = document.getElementById('card-front-bottom');
-let errorMessageName = document.getElementById('error-message-card-name');
+const cardName = document.getElementById('card-name');
+const inputName = document.getElementById('card-holder');
+const errorMessageName = document.getElementById('error-message-card-name');
 
-let cardMonth = document.getElementById('card-date-month');
-let inputMonth = document.getElementById('date-month');
-let cardYear = document.getElementById('card-date-year');
-let inputYear = document.getElementById('date-year');
-let errorMessageDates = document.getElementById('error-message-dates');
+const cardMonth = document.getElementById('card-date-month');
+const inputMonth = document.getElementById('date-month');
+const cardYear = document.getElementById('card-date-year');
+const inputYear = document.getElementById('date-year');
+const errorMessageBottom = document.getElementById('error-message-bottom');
 
-let cvc = document.getElementById('cvc');
-let inputCvc = document.getElementById('input-cvc');
-let errorMessageCvc = document.getElementById('error-message-cvc');
+const cvc = document.getElementById('cvc');
+const inputCvc = document.getElementById('input-cvc');
+
+const button = document.getElementById('button');
 
 inputNum.addEventListener('input', updateNum);
 inputName.addEventListener('input', updateName);
@@ -24,112 +23,147 @@ inputMonth.addEventListener('input', updateMonth);
 inputYear.addEventListener('input', updateYear);
 inputCvc.addEventListener('input', updateCvc);
 
-let original = cardNum.textContent;
-let original2 = cardName.textContent;
-let original3 = cardMonth.textContent;
-let original4 = cardYear.textContent;
-let original5 = cvc.textContent;
+
+let defaultNum = cardNum.textContent;
+let defaultName = cardName.textContent;
+let defaultMonth = cardMonth.textContent;
+let defaultYear = cardYear.textContent;
+let defaultCvc = cvc.textContent;
+
+let errorNum;
+let errorName;
+let errorMonth;
+let errorYear;
+let errorCvc;
+
+const errorTextNum = 'Wrong format, numbers only';
+const errorTextName = 'Wrong format, letters only';
+
+const errorChecks = [errorNum, errorName, errorMonth, errorYear, errorCvc];
 
 function updateNum(e) {
   const regex = /.{4}/g;
-  if(isNaN(e.target.value)) {
-    checkLength(e, 15);
+  if (isNaN(e.target.value)) {
+    checkLength(e, 16);
     cardNum.textContent = e.target.value.replace(regex, function (x) {
       return x + " ";
     })
-    errorMessages(errorMessageNum, inputNum, 'Wrong format, numbers only', 'red', '1px solid red');
-    }
-    else {
-      checkLength(e, 16);
-      cardNum.textContent = e.target.value.replace(regex, function (x) {
-        return x + " ";
-      })
-      errorMessages(errorMessageNum, inputNum, '', '', '');
-      
-    }
-    if(e.target.value == "") {
-      cardNum.textContent = original;
-    }
-  
+    errorMessages(errorMessageNum, inputNum, errorTextNum);
+    errorChecks[0] = true;
+  }
+  else {
+    checkLength(e, 16);
+    cardNum.textContent = e.target.value.replace(regex, function (x) {
+      return x + " ";
+    })
+    errorMessages(errorMessageNum, inputNum, '');
+    errorChecks[0] = true;
+
+  }
+  if (e.target.value == "") {
+    cardNum.textContent = defaultNum;
+  }
+
 }
 
 function updateName(e) {
   const regex = /[0-9]/g;
-  if(regex.test(e.target.value)) {
+  if (regex.test(e.target.value)) {
     checkLength(e, 20);
     cardName.textContent = e.target.value;
-    errorMessages(errorMessageName, inputName, 'Wrong format, letters only', 'red', '1px solid red');
+    errorMessages(errorMessageName, inputName, errorTextName);
+    errorChecks[1] = true;
   }
   else {
     checkLength(e, 20);
     cardName.textContent = e.target.value;
-    errorMessages(errorMessageName, inputName, '', '', '');
+    errorMessages(errorMessageName, inputName, '');
+    errorChecks[1] = false;
   }
-  if(e.target.value == "") {
-    cardName.textContent = original2;
+  if (e.target.value == "") {
+    cardName.textContent = defaultName;
   }
-  
+
 }
 
 function updateMonth(e) {
-  if(isNaN(e.target.value)) {
+  if (isNaN(e.target.value)) {
     checkLength(e, 2);
     cardMonth.textContent = e.target.value;
-    errorMessages(errorMessageDates, inputMonth, 'Wrong format, numbers only', 'red', '1px solid red');
-    }
-    else {
-      checkLength(e, 2);
-      cardMonth.textContent = e.target.value;
-      errorMessages(errorMessageDates, inputMonth, '', '', '');
-    }
-    if(e.target.value == "") {
-      cardMonth.textContent = original3;
-    }
-  
+    errorMessages(errorMessageBottom, inputMonth, errorTextNum);
+    errorChecks[2] = true;
+  }
+  else {
+    checkLength(e, 2);
+    cardMonth.textContent = e.target.value;
+    errorMessages(errorMessageBottom, inputMonth, '');
+    errorChecks[2] = false;
+  }
+  if (e.target.value == "") {
+    cardMonth.textContent = defaultMonth;
+  }
+
 }
 
 function updateYear(e) {
-  if(isNaN(e.target.value)) {
+  if (isNaN(e.target.value)) {
     checkLength(e, 2);
     cardYear.textContent = e.target.value;
-    errorMessages(errorMessageDates, inputYear, 'Wrong format, numbers only', 'red', '1px solid red');
-    }
-    else {
-      checkLength(e, 2);
-      cardYear.textContent = e.target.value;
-      errorMessages(errorMessageDates, inputYear, '', '', '');
-    }
-    if(e.target.value == "") {
-      cardYear.textContent = original4;
-    } 
+    errorMessages(errorMessageBottom, inputYear, errorTextNum);
+    errorChecks[3] = true;
+  }
+  else {
+    checkLength(e, 2);
+    cardYear.textContent = e.target.value;
+    errorMessages(errorMessageBottom, inputYear, '');
+    errorChecks[3] = false;
+  }
+  if (e.target.value == "") {
+    cardYear.textContent = defaultYear;
+  }
 }
 
 function updateCvc(e) {
-  if(isNaN(e.target.value)) {
+  if (isNaN(e.target.value)) {
     checkLength(e, 3);
     cvc.textContent = e.target.value;
-    errorMessages(errorMessageCvc, inputCvc, 'Wrong format, numbers only', 'red', '1px solid red');
-    }
-    else {
-      checkLength(e, 3);
-      cvc.textContent = e.target.value;
-      errorMessages(errorMessageCvc, inputCvc, '', '', '');
-      
-    }
-    if(e.target.value == "") {
-      cvc.textContent = original5;
-    }
-  
+    errorMessages(errorMessageBottom, inputCvc, errorTextNum);
+    errorChecks[4] = true;
+  }
+  else {
+    checkLength(e, 3);
+    cvc.textContent = e.target.value;
+    errorMessages(errorMessageBottom, inputCvc, '');
+    errorChecks[4] = false;
+
+  }
+  if (e.target.value == "") {
+    cvc.textContent = defaultCvc;
+  }
+
 }
 
 function checkLength(e, maxLength) {
-  if(e.target.value.length > maxLength) {
+  if (e.target.value.length > maxLength) {
     e.target.value = e.target.value.substring(0, maxLength);
   }
 }
 
-function errorMessages(errorContainer, error, errorMessage, color, border) {
-  errorContainer.textContent = errorMessage;
-  errorContainer.style.color = color;
-  error.style.border = border;
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+  const cardForm = document.getElementById('card-form');
+  for(let i = 0; i < errorChecks.length; i++) {
+  if (errorChecks[i] == false) {
+    cardForm.style.display = 'none';
+  } else {
+    console.log('No');
+  }
+}
+});
+function errorMessages(errorElement, inputField, errorMessage) {
+  errorElement.textContent = errorMessage;
+  errorElement.style.color = 'red';
+  inputField.style.borderColor = 'red';
+  errorElement.style.display = 'flex';
+  errorElement.style.justifyContent  = 'center';
 }
