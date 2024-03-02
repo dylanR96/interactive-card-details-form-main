99c94b3cfe65e5ff58aa411206505f9802b70892
dylanR96 <dylan.ravenson@gmail.com>
2024-03-02 17:00:42 +0100

    Finished styling and error handling

--- script.js
+++ script.js
@@ -50,116 +50,212 @@ let currentTime = new Date();
 let currentYear = currentTime.getFullYear() + 1;
 let currentMonth = currentTime.getMonth() + 1;
 currentYear = currentYear.toString().substr(-2);
+
 const errorTextNum = 'Wrong format, numbers only.';
 const errorTextName = 'Wrong format, letters only.';
-const errorTextYear = 'Please enter a valid year. Can not be current year.';
+const errorTextZeros = 'Can not be equal to or less than 0';
+const errorTextYear = 'Please enter a valid year.';
 const errotTextMonth = 'Please enter a valid month.';
 
-const errorChecks = [errorNum, errorName, errorMonth, errorYear, errorCvc];
+const errorChecks = [errorName, errorNum, errorMonth, errorYear, errorCvc];
 const trueValues = (currentValue) => currentValue == true;
 
 function updateName(e) {
-  const regex = /[0-9]/g;
+  const regex = /[^a-zA-Z ]/g;
   checkLength(e, 20);
+  cardName.textContent = e.target.value;
+  let errorParams = {
+    errorElement: errorMessageName,
+    inputField: inputName,
+    errorMessage: 'Placeholder',
+    colour: '',
+    visibility: 'hidden'
+  };
+
   if (regex.test(e.target.value)) {
-    cardName.textContent = e.target.value;
-    errorMessages(errorMessageName, inputName, errorTextName, 'red');
-    errorChecks[1] = false;
+    errorParams.errorMessage = errorTextName;
+    errorParams.colour = 'hsl(0, 100%, 66%)';
+    errorParams.visibility = 'visible';
+    errorChecks[0] = false;
   }
   else {
-    cardName.textContent = e.target.value;
-    errorMessages(errorMessageName, inputName, '', '');
-    errorChecks[1] = true;
+    errorParams.errorMessage = 'Placeholder';
+    errorParams.colour = '';
+    errorParams.visibility = 'hidden';
+    errorChecks[0] = true;
   }
   if (e.target.value == "") {
     cardName.textContent = defaultName;
-    errorChecks[1] = false;
+    errorChecks[0] = false;
   }
-
+  errorMessages(errorParams);
 }
-
 
 function updateNum(e) {
   const regex = /.{4}/g;
   checkLength(e, 16);
-  if (isNaN(e.target.value)) {
-    errorMessages(errorMessageNum, inputNum, errorTextNum, 'red');
-    errorChecks[0] = false;
-  } else if (e.target.value.length < 16) {
-    checkLength(e, 16);
-    errorChecks[0] = false;
-  }
-  else {
-    errorMessages(errorMessageNum, inputNum, '', '');
-    errorChecks[0] = true;
-  }
   cardNum.textContent = e.target.value.replace(regex, function (x) {
     return x + " ";
   })
+
+  let errorParams = {
+    errorElement: errorMessageNum,
+    inputField: inputNum,
+    errorMessage: 'Placeholder',
+    colour: '',
+    visibility: 'hidden'
+  };
+
+  if (isNaN(e.target.value)) {
+    errorParams.errorMessage = errorTextNum;
+    errorParams.colour = 'hsl(0, 100%, 66%)';
+    errorParams.visibility = 'visible';
+    errorChecks[1] = false;
+  } else if (e.target.value.length < 16) {
+    errorParams.errorMessage = 'Placeholder';
+    errorParams.colour = '';
+    errorParams.visibility = 'hidden';
+    errorChecks[1] = false;
+  } else if (e.target.value <= 0) {
+    errorParams.errorMessage = errorTextZeros;
+    errorParams.colour = 'hsl(0, 100%, 66%)';
+    errorParams.visibility = 'visible';
+    errorChecks[1] = false;
+  } else {
+    errorParams.errorMessage = 'Placeholder';
+    errorParams.colour = '';
+    errorParams.visibility = 'hidden';
+    errorChecks[1] = true;
+  }
   if (e.target.value == "") {
     cardNum.textContent = defaultNum;
+    errorChecks[1] = false;
   }
-
+  errorMessages(errorParams);
 }
 
 function updateMonth(e) {
   checkLength(e, 2);
+  cardMonth.textContent = e.target.value;
+
+  let errorParams = {
+    errorElement: errorMessageBottom,
+    inputField: inputMonth,
+    errorMessage: 'Placeholder',
+    colour: '',
+    visibility: 'hidden'
+  };
+
   if (isNaN(e.target.value)) {
-    cardMonth.textContent = e.target.value;
-    errorMessages(errorMessageBottom, inputMonth, errorTextNum, 'red');
+    errorParams.errorMessage = errorTextNum;
+    errorParams.colour = 'hsl(0, 100%, 66%)';
+    errorParams.visibility = 'visible';
     errorChecks[2] = false;
   } else if (parseInt(e.target.value, 10) > 12) {
-    cardMonth.textContent = e.target.value;
-    errorMessages(errorMessageBottom, inputMonth, errotTextMonth, 'red');
-    errorChecks[0] = false;
-  }
-  else {
-    cardMonth.textContent = e.target.value;
-    errorMessages(errorMessageBottom, inputMonth, '', '');
+    errorParams.errorMessage = errotTextMonth;
+    errorParams.colour = 'hsl(0, 100%, 66%)';
+    errorParams.visibility = 'visible';
+    errorChecks[2] = false;
+  } else if (e.target.value <= 0) {
+    errorParams.errorMessage = errorTextZeros;
+    errorParams.colour = 'hsl(0, 100%, 66%)';
+    errorParams.visibility = 'visible';
+    errorChecks[2] = false;
+  } else {
+    errorParams.errorMessage = 'Placeholder';
+    errorParams.colour = '';
+    errorParams.visibility = 'hidden';
     errorChecks[2] = true;
   }
   if (e.target.value == "") {
+    errorParams.errorMessage = 'Placeholder';
+    errorParams.colour = '';
+    errorParams.visibility = 'hidden';
     cardMonth.textContent = defaultMonth;
+    errorChecks[2] = false;
   }
+  errorMessages(errorParams);
 }
 
 function updateYear(e) {
   checkLength(e, 2);
+  cardYear.textContent = e.target.value;
+
+  let errorParams = {
+    errorElement: errorMessageBottom,
+    inputField: inputYear,
+    errorMessage: 'Placeholder',
+    colour: '',
+    visibility: 'hidden'
+  };
+
   if (isNaN(e.target.value)) {
-    cardYear.textContent = e.target.value;
-    errorMessages(errorMessageBottom, inputYear, errorTextNum, 'red');
+    errorParams.errorMessage = errorTextNum;
+    errorParams.colour = 'hsl(0, 100%, 66%)';
+    errorParams.visibility = 'visible';
     errorChecks[3] = false;
   } else if (parseInt(e.target.value, 10) < currentYear) {
-    cardYear.textContent = e.target.value;
-    errorMessages(errorMessageBottom, inputYear, errorTextYear, 'red');
-    errorChecks[0] = false;
+    errorParams.errorMessage = errorTextYear;
+    errorParams.colour = 'hsl(0, 100%, 66%)';
+    errorParams.visibility = 'visible';
+    errorChecks[3] = false;
+  } else if (e.target.value <= 0) {
+    errorParams.errorMessage = errorTextZeros;
+    errorParams.colour = 'hsl(0, 100%, 66%)';
+    errorParams.visibility = 'visible';
+    errorChecks[3] = false;
   } else {
-    cardYear.textContent = e.target.value;
-    errorMessages(errorMessageBottom, inputYear, '', '');
+    errorParams.errorMessage = 'Placeholder';
+    errorParams.colour = '';
+    errorParams.visibility = 'hidden';
     errorChecks[3] = true;
   }
   if (e.target.value == "") {
+    errorParams.errorMessage = 'Placeholder';
+    errorParams.colour = '';
+    errorParams.visibility = 'hidden';
     cardYear.textContent = defaultYear;
+    errorChecks[3] = false;
   }
+  errorMessages(errorParams);
 }
 
 function updateCvc(e) {
   checkLength(e, 3);
-  if (isNaN(e.target.value)) {
-    cvc.textContent = e.target.value;
-    errorMessages(errorMessageBottom, inputCvc, errorTextNum, 'red');
-    errorChecks[4] = false;
-  }
-  else {
-    cvc.textContent = e.target.value;
-    errorMessages(errorMessageBottom, inputCvc, '', '');
-    inputCvc.style.borderColor = '';
-    errorChecks[4] = true;
+  cvc.textContent = e.target.value;
 
+  let errorParams = {
+    errorElement: errorMessageBottom,
+    inputField: inputCvc,
+    errorMessage: 'Placeholder',
+    colour: '',
+    visibility: 'hidden'
+  };
+
+  if (isNaN(e.target.value)) {
+    errorParams.errorMessage = errorTextNum;
+    errorParams.colour = 'hsl(0, 100%, 66%)';
+    errorParams.visibility = 'visible';
+    errorChecks[4] = false;
+  } else if (e.target.value <= 0) {
+    errorParams.errorMessage = errorTextZeros;
+    errorParams.colour = 'hsl(0, 100%, 66%)';
+    errorParams.visibility = 'visible';
+    errorChecks[4] = false;
+  } else {
+    errorParams.errorMessage = 'Placeholder';
+    errorParams.colour = '';
+    errorParams.visibility = 'hidden';
+    errorChecks[4] = true;
   }
   if (e.target.value == "") {
-    cvc.textContent = defaultCvc;
+    errorParams.errorMessage = 'Placeholder';
+    errorParams.colour = '';
+    errorParams.visibility = 'hidden';
+    cardYear.textContent = defaultYear;
+    errorChecks[4] = false;
   }
+  errorMessages(errorParams);
 }
 
 function checkLength(e, maxLength) {
@@ -176,8 +272,14 @@ btnConfirm.addEventListener('click', (e) => {
       cardForm.style.display = 'none';
       completedState.style.display = 'flex'
     } else {
+      errorMessageForm.style.visibility = 'visible';
       errorMessageForm.textContent = 'All fields must be filled with valid info.'
       errorMessageForm.style.color = 'Red';
+      for (let i = 0; i < inputFields.length; i++) {
+        inputFields[i].addEventListener('keydown', () => {
+          errorMessageForm.style.visibility = 'hidden';
+        })
+      }
     }
   }
 });
@@ -186,7 +288,7 @@ btnContinue.addEventListener('click', (e) => {
   e.preventDefault();
   completedState.style.display = 'none'
   cardForm.style.display = 'flex';
-  for(let i = 0; i < errorChecks.length; i++) {
+  for (let i = 0; i < errorChecks.length; i++) {
     errorChecks[i] = false;
   }
   for (let i = 0; i < inputFields.length; i++) {
@@ -195,10 +297,8 @@ btnContinue.addEventListener('click', (e) => {
   }
 })
 
-function errorMessages(errorElement, inputField, errorMessage, colour) {
-  errorElement.textContent = errorMessage;
-  errorElement.style.color = colour;
+function errorMessages({ errorElement, inputField, errorMessage, colour, visibility }) {
+  errorElement.innerHTML = errorMessage;
   inputField.style.borderColor = colour;
-  errorElement.style.display = 'flex';
-  errorElement.style.justifyContent = 'center';
+  errorElement.style.visibility = visibility;
 }
